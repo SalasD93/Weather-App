513cf9697e66d34a16c351ec571c2b8f4082e266
Diana Salas <salasd10418@gmail.com>
2021-08-30 23:06:33 -0400

    removed unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 var body = document.body;
-
 var container = document.querySelector('.container');
 
 // This function accesses the APIs and displays relevant information to the page
 var citiesList = JSON.parse(localStorage.getItem("searched")) || [];
+// This sets the value for page revisit to last city searched or NY on first visit
 var cityValue = citiesList[0] || "New York";
 $('#add-city').on('click', (event) => {
     event.preventDefault();
@@ -14,37 +14,34 @@ $('#add-city').on('click', (event) => {
     addCityToHistory(cityValue);
     displayHistory(cityValue);
 });
-// how to add something to front of list
 
 function addCityToHistory(cityValue) {
-    //add city value to city list
+    // Add city value to city list
     // This prevents duplicates in array for buttons
     if (citiesList.indexOf(cityValue) === -1) {
         citiesList.unshift(cityValue);
     }
     // Added to prevent inifinite array / limit button ouputs / readd city to recent searches due to indexOf preventing addition of dupicates
     citiesList.splice(5);
-    //save to localStorage
+    // Save to localStorage
     localStorage.setItem("searched", JSON.stringify(citiesList));
 }
 
 function displayHistory(cityValue) {
-    // get the list of cities from the history
-    // const searchedCities = localStorage.getItem("searched").split(",");
-    console.log(citiesList);
-        //loop for every city in list
-        document.querySelector("#city-list").innerHTML = "";
-        for (let c = 0; c < citiesList.length; c++) {
-            if (citiesList[c] !== '') {
-                //make button
-                document.querySelector("#city-list").innerHTML += `<button type="button" class="btn btn-info mx-1 my-2 cityBtn">${citiesList[c]}</button>`;
-            }
+    // Get the list of cities from the history
+    // Loop for every city in list
+    document.querySelector("#city-list").innerHTML = "";
+    for (let c = 0; c < citiesList.length; c++) {
+        if (citiesList[c] !== '') {
+            //make button
+            document.querySelector("#city-list").innerHTML += `<button type="button" class="btn btn-info mx-1 my-2 cityBtn">${citiesList[c]}</button>`;
         }
-        // cityBtn click is same as submit: getData with city name for button text
-        $(".cityBtn").on('click', (cityValue) => {
-            // cityValue = this.$(".cityBtn").text()
-            getData(cityValue.target.innerText);
-        });
+    }
+    // cityBtn click is same as submit: getData with city name for button text
+    $(".cityBtn").on('click', (cityValue) => {
+        // cityValue = this.$(".cityBtn").text()
+        getData(cityValue.target.innerText);
+    });
 }
 // This function holds the api calls and displays the api information
 async function getData(cityValue) {
@@ -55,26 +52,19 @@ async function getData(cityValue) {
     const api_url2 = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=imperial&exclude=minutely&appid=54e653cb1cf1c02994d2f2adcdfa1673`;
     const response2 = await fetch(api_url2);
     const weather2 = await response2.json();
-    console.log(weather2);
     const lat = weather2.coord.lat;
     const lon = weather2.coord.lon;
 
-    
     // This api gets information based off lat lon information from city api
     const api_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely&appid=54e653cb1cf1c02994d2f2adcdfa1673`;
     const response = await fetch(api_url);
     const weather = await response.json();
-    console.log(weather);
     
     // This section gets info for current weather and displays it
     const city = weather2.name;
-    console.log(city);
     const currentDate = moment.unix(weather2.dt).format("MM/DD/YYYY");
-    console.log(currentDate);
     const currentIcon = weather.current.weather[0].icon;
-    console.log(currentIcon);
     const curIconUrl = `https://openweathermap.org/img/wn/${currentIcon}.png`;
-    console.log(curIconUrl);
     const curIconImg = document.createElement('img');
     $(curIconImg).attr('src', curIconUrl);
     const dispCity = document.createElement('h1');
@@ -82,17 +72,12 @@ async function getData(cityValue) {
     $(dispCity).append(curIconImg);
     $("#current-weather").append(dispCity);
     const currentTemp = 'Temp: ' + weather2.main.temp + '°F' + '<br>';
-    // $(currentTemp).attr('style', "font-size: 2rem");
-    console.log(currentTemp);
     $("#current-weather").append(currentTemp);
     const currentWind = 'Wind: ' + weather2.wind.speed + 'MPH' + '<br>';
-    // $(currentWind).attr('style', "font-size: 2rem");
-    console.log(currentWind);
     $("#current-weather").append(currentWind);
     const currentHumidity = 'Humidity: ' + weather2.main.humidity + '%' + '<br>';
-    // $(currentHumidity).attr('style', "font-size: 2rem");
-    console.log(currentHumidity);
     $("#current-weather").append(currentHumidity);
+    // This changes the color for the index
     var uviId = "";
     if (weather.current.uvi <= 2) {
         uviId = "uviG";
@@ -105,37 +90,22 @@ async function getData(cityValue) {
     }
     const UVI = `<span id=${uviId}>${weather.current.uvi}</span>`;
     const currentUV = 'UV Index: ' + UVI;
-    console.log(currentUV);
     $("#current-weather").append(currentUV);
 
-    // need empty array for cities
-        // every time a city is input a button is created and stored in LS
-        // need to = button text to input val on click
-        // var answers = document.createElement('div');
-        // $(answers).attr('id', "cities");
-        // $(answers).addClass("buttons is-centered");
-        // $(question).text("What are you in the mood for?");
-        
-        // This loops through daily array for first 5 items / creates weather cards / displays relevant information
+    // This loops through daily array for first 5 items / creates weather cards / displays relevant information
     for (var i = 1; i < 6; i++) {
         const daily = weather.daily;
         // This converts unix date to standard date
         const date = moment.unix(daily[i].dt).format("MM/DD/YYYY");
-        console.log(date);
         const icon = daily[i].weather[0].icon;
-        console.log(icon);
         const temp = daily[i].temp.day;
-        console.log(temp);
         const windSpeed = daily[i].wind_speed;
-        console.log(windSpeed);
         const humidity = daily[i].humidity;
-        console.log(humidity);
         const iconUrl= `https://openweathermap.org/img/wn/${icon}.png`;
-        console.log(iconUrl);
+
         // This section creates the weather cards and adds the content
         var weatherCard = document.createElement('div');
         $(weatherCard).attr('style', "opacity: 0.65");
-        // $(weatherCard).attr('style', "height: 250px");
         $(weatherCard).addClass("card col-sm-2");
         $("#display-cards").append(weatherCard);
         var headerDate = document.createElement('h5');
